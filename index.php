be63e48f45e1141413816ddc017ae9b809f89725
Andrian Lau <34380711+LAO47@users.noreply.github.com>
2019-03-17 21:05:37 +0700

    Create index.php
    tambah index dulu hhh

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+    <style>
+        .flex-container
+        {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            background-color: #8a44ff;
+        }
+
+        .flex-container > div 
+        {
+            border-style: outset;
+            margin: 10px;
+            padding: 20px;
+            font-size: 30px;
+        }
+    </style>
+
+</head>
+<body>
+<nav class="flex-container">
+  <div>Home</div>
+  <div>Pengolahan Data</div>
+  <div>Tentang Kami</div>
+  <div>Kontak</div>
+</nav>
+
+</body>
+</html>
